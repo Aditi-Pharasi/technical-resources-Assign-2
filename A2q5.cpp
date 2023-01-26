509cadce2e3aacf9d2328ed79e07c2e0716ab7fb
Aditi-Pharasi <116096105+Aditi-Pharasi@users.noreply.github.com>
2023-01-26 16:19:59 +0530

    Add files via upload

--- A2q5.cpp
+++ A2q5.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv){
+int main(){
     int n;
+    cout<<"enter the number of rows:";
     cin >> n;
+    int sp=n/2;
+    int str=1;
     for(int i=1;i<=n;i++){
-        for (int j=1;j<n-2*i)
+        for (int j=1;j<=sp;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=str;k++){
+            cout<<"*";
+        }
+        if(i<(n+1)/2){
+            sp--;
+            str=str+2;
+        }
+        else{
+            sp++;
+            str=str-2;
+        }
+        cout<<endl;
     }
-    //write your code here
-
-    }
-}
+ }
